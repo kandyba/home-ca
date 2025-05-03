602118e98838684136f9c40054a4200bdee94f9a
slivas <polesskyvlad@gmail.com>
2025-05-04 02:54:05 +0300

    Header (all states for mobile and desktop), update primary button's styles, mobile map section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ export default {
         primary: "#FF603F",
         secondary: "#293464",
         darkText: "#1F1F1F",
+        darkText3: "#B6B6B6",
         lightText: "#949494",
         lightGray: "#F5F5F5",
         mediumGray: "#E0E0E0",
+        orange2: "#C94629",
+        orange4: "#FF785C",
       },
       fontFamily: {
         sans: ['"Albert Sans"', 'sans-serif'],
@@ -27,6 +30,18 @@ export default {
           xl: '1280px',
           '2xl': '1440px',
         },
+      },
+      spacing: {
+        '4.5': '1.125rem',
+        '5.5': '1.375rem',
+        '7.5': '1.875rem',
+      },
+      letterSpacing: {
+        tighter: '-0.42px',
+        tight: '-0.36px',
+      },
+      boxShadow: {
+        custom: '0 8px 16px 0px rgba(0, 0, 0, 0.12)',
       },
     },
   },
